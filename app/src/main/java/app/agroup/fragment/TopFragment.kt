6326c933f6efb63bc8209308e4da3d4570ad4c22
kanetsuku-hide <kanetsuku-hide@dmm.com>
2021-02-14 18:00:38 +0900

    Navigationを用いた画面遷移の作成

--- app/src/main/java/app/agroup/fragment/TopFragment.kt
+++ app/src/main/java/app/agroup/fragment/TopFragment.kt
@@ -18,15 +18,15 @@ class TopFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        var button = view.findViewById<Button>(R.id.button_first)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_TopFragment_to_resultFragment)
         }
     }
 }
